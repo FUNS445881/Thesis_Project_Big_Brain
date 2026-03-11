3d3d8939ed26839060c786e352987424c0ca7017
sion703 <862336319@qq.com>
2026-03-11 18:54:39 +0800

    基本流程完善

--- config.py
+++ config.py
@@ -6,8 +6,12 @@ JUDGE_LLM_API = ""
 VLM_API = ""
 
 # RAG
-RAG_MODEL = ""
-RAG_SIMILARITY_THRESHOLD = 0.75
+RAG_MODEL = "/home/xiao/.cache/huggingface/hub/models--sentence-transformers--all-mpnet-base-v2/snapshots/e8c3b32edf5434bc2275fc9bab85f82640a19130"
+#RAG_MODEL = "/home/xiao/thesis_ws/rag_model/models--sentence-transformers--all-mpnet-base-v2/snapshots/e8c3b32edf5434bc2275fc9bab85f82640a19130"
+RAG_SIMILARITY_THRESHOLD = 0.3
+HISTORY_PATH = "big_brain/memory/rag_history.json"
+
 
 # 物体默认长宽高(cm)
 BOX_SIZE = (50, 50, 50)
+
