5b750875a67812979491336c575ddaeb6e2202c1
sion703 <862336319@qq.com>
2026-03-13 12:52:48 +0800

    parse_object初稿

--- config.py
+++ config.py
@@ -30,6 +30,8 @@ VLM_API_KEY = ZHIPU_API_KEY
 VLM_API_BASE_URL = ZHIPU_BASE_URL
 VLM_MODEL = ZHIPU_VLM_MODEL
 
+MAX_REPLAN_TIMES = 3
+
 # RAG
 RAG_MODEL = "/home/xiao/.cache/huggingface/hub/models--sentence-transformers--all-mpnet-base-v2/snapshots/e8c3b32edf5434bc2275fc9bab85f82640a19130"
 #RAG_MODEL = "/home/xiao/thesis_ws/rag_model/models--sentence-transformers--all-mpnet-base-v2/snapshots/e8c3b32edf5434bc2275fc9bab85f82640a19130"
@@ -39,3 +41,6 @@ HISTORY_PATH = "big_brain/memory/rag_history_old.json"
 
 # 物体默认长宽高(cm)
 BOX_SIZE = (50, 50, 50)
+
+# YOLO Detect Objects
+YOLO_DETECT_OBJECTS = ["desk","chair","bottle","apple","banana"]
